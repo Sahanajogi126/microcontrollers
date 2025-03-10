6e4dda4823dd016e5e7eb63c77f8f29acc8ecef5
Sahanajogi126 <sahana.23cs126@sode-edu.in>
2025-03-10 21:21:54 +0530

    prg3.asm

---
+++ prg3.asm
@@ -0,0 +1,37 @@
+AREA prg3,CODE,READONLY
+START
+         MOV R0,#0
+         MOV R1,#10
+	 
+         ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 ADD R0,R1
+	 SUB R1,#1
+	 
+	 BX LR
+END
